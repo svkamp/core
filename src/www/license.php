5768678f1e7e3aaeee8326329c4fbf3df2e3061a
Scott K <outbackdingo@gmail.com>
2016-12-24 20:38:24 -0500

    updated license / default urls / Copyright

--- src/www/license.php
+++ src/www/license.php
@@ -38,7 +38,7 @@ include("head.inc");
   <div class="container-fluid ">
     <div class="row">
       <section class="col-xs-11">
-        <p><a href="https://opnsense.org/" target="_blank">OPNsense&reg;</a></span> is Copyright &copy; <?=$g['product_copyright_years'];?> <?= $g['product_copyright_owner'] ?><br>All rights reserved.</p>
+        <p><a href="https://www.optimcloud.com/" target="_blank">OptimStor&reg;</a></span> is Copyright &copy; <?=$g['product_copyright_years'];?> <?= $g['product_copyright_owner'] ?><br>All rights reserved.</p>
         <p>Redistribution and use in source and binary forms, with or without modification, are permitted provided that the following conditions are met:</p>
         <p><ol><li>Redistributions of source code must retain the above copyright notice, this list of conditions and the following disclaimer.</li>
         <li>Redistributions in binary form must reproduce the above copyright notice, this list of conditions and the following disclaimer in the documentation and/or other materials provided with the distribution.</li></ol></p>
@@ -52,11 +52,12 @@ include("head.inc");
         WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR
         OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF
         ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.</p>
-        <p>OPNsense is based on <a href="http://www.freebsd.org" target="_blank">FreeBSD</a> Copyright &copy; The FreeBSD Project. All rights reserved.</p>
+        <p>OptimStor is based on <a href="http://www.freebsd.org" target="_blank">FreeBSD</a> Copyright &copy; The FreeBSD Project. All rights reserved.</p>
+        <p>OptimStor is a fork of <a href="http://www.opnsense.org" target="_blank">OPNSense&reg;</a> <small>(Copyright &copy; OPNsense is an OSS project © Deciso B.V. 2015-2016 - All rights reserved.)</small>.</p>
         <p>OPNsense is a fork of <a href="http://www.pfsense.org" target="_blank">pfSense&reg;</a> <small>(Copyright &copy; 2004-2014 Electric Sheep Fencing, LLC. All rights reserved.)</small> a fork from <a href="http://m0n0.ch/wall" target="_blank">m0n0wall&reg;</a> <small>(Copyright &copy; 2002-2013 Manuel Kasper)</small>.</p>
-        <p>OPNsense includes various freely available software packages and ports.
+        <p>OptimStor includes various freely available software packages and ports.
         The incorporated third party tools are listed <a href="/ui/core/firmware/#packages">here</a>.</p>
-        <p>The authors of OPNsense would like to thank all contributors for their efforts.</p>
+        <p>The authors of OptimStor would like to thank all contributors for their efforts.</p>
       </section>
     </div>
   </div>
